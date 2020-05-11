698ad68348c5a62ec69d438005652d410cd8edaf
creasylai19 <creasylai19@gmail.com>
2020-05-12 03:24:31 +0800

    添加网关Gateway

---
+++ spring-gateway/src/main/java/com/creasy/gateway/GatewayApplication9002.java
@@ -0,0 +1,13 @@
+package com.creasy.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication9002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication9002.class, args);
+    }
+
+}
