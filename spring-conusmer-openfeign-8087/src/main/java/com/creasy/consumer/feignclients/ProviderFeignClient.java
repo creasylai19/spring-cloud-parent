eebd0911736dcb13ec5b5c18bb153637bf4715f5
creasylai19 <creasylai19@gmail.com>
2020-06-02 13:02:11 +0800

    添加sleuth和zipkin

--- spring-conusmer-openfeign-8087/src/main/java/com/creasy/consumer/feignclients/ProviderFeignClient.java
+++ spring-conusmer-openfeign-8087/src/main/java/com/creasy/consumer/feignclients/ProviderFeignClient.java
@@ -2,10 +2,12 @@ package com.creasy.consumer.feignclients;
 
 import com.creasy.pojo.Article;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Primary
 @FeignClient(value = "SPRING-PROVIDER", fallback = ProviderFeignClientFallback.class, path = "/article")
 public interface ProviderFeignClient {
 
